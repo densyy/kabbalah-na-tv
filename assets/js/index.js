5f1ced217a23042867fd0e5e5634e34540e7bb41
densyy <densyy@hotmail.com>
2025-11-29 08:34:24 -0300

    feat: recebendo aulas

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,31 @@
+/* ---- Envs ---- */
+
+const BASE_URL = 'https://kabbalahmedia.info/backend'
+let lessons = []
+
 /* ---- OnMount ---- */
 
-loading()
+main()
 
 /* ---- Functions ---- */
 
-function loading () {
+async function main () {
+  await getLessons()
+  stopLoading()
+}
+
+function stopLoading () {
   setTimeout(() => {
     const loading = document.getElementById('loading')
     if (loading) loading.style.display = 'none'
-  }, 5000)
+  }, 500)
+}
+
+async function getLessons () {
+  try {
+    const response = await fetch(`${BASE_URL}/lessons?page_no=1&page_size=10&withViews=true&content_type=DAILY_LESSON&ui_language=pt&content_languages=pt`)
+    lessons = await response.json()
+  } catch (error) {
+    console.log('Error fetching lessons:', error)
+  }
 }

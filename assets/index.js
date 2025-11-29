ae83bf538bdd162bd8e7213f03110b850e2d7c93
densyy <densyy@hotmail.com>
2025-11-29 08:22:48 -0300

    feat: add loading

--- assets/index.js
+++ assets/index.js
@@ -1,1 +1,12 @@
-console.log("Hello from Kabbalah na TV!")
+/* ---- OnMount ---- */
+
+loading()
+
+/* ---- Functions ---- */
+
+function loading () {
+  setTimeout(() => {
+    const loading = document.getElementById('loading')
+    if (loading) loading.style.display = 'none'
+  }, 5000)
+}
